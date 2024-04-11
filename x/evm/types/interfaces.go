3b904fee360aab066663a39e5073ba3305f0b71b
Vu Ngoc Quang <quang.vn@outlook.com>
2024-04-11 15:03:09 +0700

    create eth account for contract

--- x/evm/types/interfaces.go
+++ x/evm/types/interfaces.go
@@ -19,6 +19,7 @@ import (
 // AccountKeeper defines the expected account keeper interface
 type AccountKeeper interface {
 	NewAccountWithAddress(ctx sdk.Context, addr sdk.AccAddress) authtypes.AccountI
+	NewAccount(ctx sdk.Context, acc authtypes.AccountI) authtypes.AccountI
 	GetModuleAddress(moduleName string) sdk.AccAddress
 	GetAllAccounts(ctx sdk.Context) (accounts []authtypes.AccountI)
 	IterateAccounts(ctx sdk.Context, cb func(account authtypes.AccountI) bool)
